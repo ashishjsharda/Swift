298d23f831fc1a28efc1016f6a89a8be352e3345
Ashish S <ashishjsharda@gmail.com>
2019-09-01 10:00:59 -0400

    Swift Getting Started
    Swift Getting Started

---
+++ example.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+var str = "Hello, playground"
+var i=5
+for j in 0...7
+{
+    i+=j
+}
+var str2=" Hello Sai"
+let name:String="Paul"
+let age:Int=27
+var ages:Int=28
+//Tuples
+let player=("Ashish","AJ","Shalu")
+print(player.0)
+//Tuples
+let players=(name:"Sai",age:5000)
+print(players.name)
+//Arrays
+var arrs=[1,2,3,4]
+arrs.count//Count of number of elements
+//While loop
+var i1=0
+while i1<5{
+    print(i1)
+    i1=i1+1;
+}
+var theBeatles=["Paul","Jean","Michael"]
+let greet={
+    
+}
+
+
