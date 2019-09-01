8618b8b7478048ba39d2e516280d86371602bde4
Ashish S <ashishjsharda@gmail.com>
2019-09-01 09:13:49 -0400

    Using Functions in Swift
    Using Functions in Swift

---
+++ functions.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+print("Hello World")
+let greet={
+    print("Hello World")
+}
+greet()
+func greets(name: String,age:Int) { 
+    print("Hello, \(name)  Age seen is \(age)!" ) 
+} 
+greets(name:"Jim",age:100) // Hello, Jim! 
