510231f37cb5e38f270be6946e8d23f7dc758ff4
Ashish S <ashishjsharda@gmail.com>
2019-09-08 16:20:00 -0400

    functionsexample.swift
    functionsexample.swift

---
+++ functionsexample.swift
@@ -0,0 +1,15 @@
+import UIKit
+//Function
+
+//Return type void
+func display()
+{
+    print("Hi")
+}
+//Return type String
+func studentName()->String
+    {
+    return "Amol"
+}
+print(display())
+print(studentName())
