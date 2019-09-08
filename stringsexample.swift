7c113866023b4533acd38caf2ca373376a69f687
Ashish S <ashishjsharda@gmail.com>
2019-09-08 11:39:28 -0400

    stringexample.swift

---
+++ stringsexample.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+var str1=String("This is a test")
+print(str1)
+let stringC="""
+This is test string quite long
+and this is for demo purpose
+"""
+print(stringC)
