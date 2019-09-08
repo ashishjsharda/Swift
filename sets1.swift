c7b7188b831071e9c68822c55a7b71d7b8db38e4
Ashish S <ashishjsharda@gmail.com>
2019-09-08 13:30:31 -0400

    sets.swift
    sets.swift

---
+++ sets1.swift
@@ -0,0 +1,15 @@
+import UIKit
+//Sets
+
+var sets=Set<Character>()
+sets.insert("e")
+sets.insert("b")
+sets.insert("A")
+sets.insert("B")
+sets.insert("C")
+print(sets.count)
+for items in sets.sorted(){
+    print(items)
+}
+
+
