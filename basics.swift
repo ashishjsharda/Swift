c8f991be7b5671648f6cd818367997e914be4364
Ashish S <ashishjsharda@gmail.com>
2020-03-23 18:59:46 -0400

    Create basics.swift

---
+++ basics.swift
@@ -0,0 +1,16 @@
+import UIKit
+//Basic String
+var str = "Hello, playground"
+print(str)
+//Define Type
+var d:Double=20.0
+print(d)
+//Tuple
+var tup1=(10,20,"Ashish")
+print(tup1)
+//Using Optional
+var op:String?
+print(op)
+//Literals
+let a=10
+print(a)
