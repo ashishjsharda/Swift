ce2f37d6b3f60071dc798e94b6e521844bc8cac7
Ashish S <ashishjsharda@gmail.com>
2020-08-12 07:32:25 -0400

    Create optionals.swift

---
+++ optionals.swift
@@ -0,0 +1,16 @@
+var name:String?
+if name != nil {
+    print(name)
+}
+else {
+    print ("Name is null ")
+}
+
+
+var myInt :Int? = 10
+if myInt != nil {
+    print(myInt)
+}
+else{
+    print("No assigned value")
+}
